b29148314598d503e906c089954f987bce57115c
Arindam Chowdhury <carindam24@gmail.com>
2024-05-31 19:04:59 +0530

    some fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,36 +3,47 @@ import styled from "../styles/Home.module.css";
 
 export default function HomePage() {
   const [verifyOTP, setVerifyOTP] = useState(false);
-  return (
-     <>
-      <div className={styled.main_container}>
-       <form className={styled.form_container} action="">
-         <div className={styled.input_content}>
-           <label htmlFor="phone_number">Phone</label>
-           <input className={styled.input_class} type="text" name="phone_number" id="phoneNumber" />
-        </div>
-        <button className={styled.submit_button} type="submit">Send OTP</button>
-      </form>
-     </div>
-     <div className={styled.main_container}>
-      <form className={styled.form_container} action="">
-      <div className={styled.input_content}>
-        <label htmlFor="opt_number">Enter OTP</label>
-        <input
-          className={styled.input_class}
-          type="text"
-          name="otp_number"
-          id="otpNumber"
-        />
-      </div>
-      <button className={styled.submit_button} type="submit">
-        Verify OTP
-      </button>
-    </form>
-  </div> 
-  </>
-    
 
-    
+  function handleVerifyOTPClick() {
+    setVerifyOTP(!verifyOTP);
+  }
+  return (
+    <>
+      {verifyOTP == false ? (
+        <div className={styled.main_container}>
+          <form className={styled.form_container} action="">
+            <div className={styled.input_content}>
+              <label htmlFor="phone_number">Phone</label>
+              <input
+                className={styled.input_class}
+                type="text"
+                name="phone_number"
+                id="phoneNumber"
+              />
+            </div>
+            <button onClick={() => {handleVerifyOTPClick()}} className={styled.submit_button} type="submit">
+              Send OTP
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className={styled.main_container}>
+          <form className={styled.form_container} action="">
+            <div className={styled.input_content}>
+              <label htmlFor="opt_number">Enter OTP</label>
+              <input
+                className={styled.input_class}
+                type="text"
+                name="otp_number"
+                id="otpNumber"
+              />
+            </div>
+            <button className={styled.submit_button} type="submit">
+              Verify OTP
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
